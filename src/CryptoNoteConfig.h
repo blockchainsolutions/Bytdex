17086812f9ee32d00d67de1135d71a77b6cd7c13
Blockchain Solutions <35883225+blockchainsolutions@users.noreply.github.com>
2018-06-14 17:22:36 +1000

    new
    new

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,7 +106,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  //"166.162.36.214:17501",
+  //"149.28.55.201:17501",
   //"your_seed_ip2.com:8080",
 };
 
